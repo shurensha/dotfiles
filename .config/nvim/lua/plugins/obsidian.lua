19dd3c09a96fbf0ac011d7c5d57408673e5c339e
Shuren <shurensha@gmail.com>
2024-07-06 02:51:58 +0800

    update for arch config

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -172,7 +172,7 @@ return {
 
 		-- Optional, for templates (see below).
 		templates = {
-			folder = "templates",
+			folder = "~/Documents/notes/templates",
 			date_format = "%Y-%m-%d",
 			time_format = "%H:%M",
 			-- A map for custom variables, the key should be the variable and the value a function
